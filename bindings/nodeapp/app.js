b030a6688e607f67d8737b4a016e3ff3b3653aaa
Martin Raepple <maraeppl@microsoft.com>
2020-10-22 11:45:42 +0200

    Changed message

--- bindings/nodeapp/app.js
+++ bindings/nodeapp/app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 const port = 3000;
 
 app.post('/sample-topic', (req, res) => {
-    console.log("Hello from Kafka!");
+    console.log("Hello!");
     console.log(req.body);
     res.status(200).send();
 });
